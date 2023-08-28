c9f2e66f48a073de322573b5f5ff5779f129c7c4
Lucas Souders <souderslucas@gmail.com>
2023-08-28 16:22:08 -0400

    First commit?

---
+++ stats.py
@@ -0,0 +1,10 @@
+###
+# This script will take input in the form of an excel spread sheet containing people and their weekly stats as well as the number of weeks that 
+# they have attended. We will store the data into a pandas DataFrame, and calculate the following:
+#   1. Average of the players' best 5 weeks.
+#   2. The players' weekly average.
+# We will then sort all of the players based on their 5-weeks best average, and dump this information into an excel spreadsheet
+# or some other format that will be easily readable. 
+#
+# Author: Lucas Souders
+###
